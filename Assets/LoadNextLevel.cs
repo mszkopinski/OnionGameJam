934b2cfb903918a9df08efa73e157df75d753e23
Paolo93 <ratalewski@wp.pl>
2019-02-23 20:43:12 +0100

    add win

---
+++ Assets/LoadNextLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void LevelComplete()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
