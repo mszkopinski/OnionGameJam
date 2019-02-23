0c610b5b96e13eb76b25fe74e13bb37bd35ca064
Michał <michal_szkopinski@op.pl>
2019-02-23 23:07:26 +0100

    Some changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -80,6 +80,7 @@ public class GameManager : MonoSingleton<GameManager>
             var moveQueue = currentLayer.EnemiesMoveQueue;
             if (moveQueue.Count <= 1)
             {
+                lastMoveIndex = 0;
                 LayerManager.Instance.PopLayer();
                 return true;
             }
