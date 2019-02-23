784d369b71625d3c4d223734bc355e827abd6cb0
Paolo93 <ratalewski@wp.pl>
2019-02-23 13:57:11 +0100

    Add Scene GUI and code GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,20 @@
 ﻿using Utils;
+using TMPro;
+
 
 public class GameManager : MonoSingleton<GameManager>
 {
-    
+    public int rounds = 0;
+    public TextMeshProUGUI RoundsLabel = null;
+
+    public void Start()
+    {
+        if(RoundsLabel)
+        {
+            RoundsLabel.text = "ROUNDS: " + rounds;
+        }
+        
+    }
+
+
 }
