0a710815a83f84fd3b9708bdfa78b7b46005b8cd
Maciej <gortatm@gmail.com>
2019-02-24 13:25:08 +0100

    Working on AI

--- Assets/Scripts/Enemies/Wolf.cs
+++ Assets/Scripts/Enemies/Wolf.cs
@@ -1,11 +1,52 @@
 using UnityEngine;
+using Layers;
 
 public class Wolf : Entity
 {
     public override void OnMoveStarted()
     {
         base.OnMoveStarted();
+        Debug.Log("-1");
         if (IsMoving == false) return;
-        CurrentTarget = CurrentPosition + new Vector2Int(0, 1);
+        //CurrentTarget = CurrentPosition + new Vector2Int(0, 1);
+        if (CheckIfPlacerIsClose() != new Vector2Int(0, 0)) {
+        	Debug.Log("0");
+        	CurrentTarget = CheckIfPlacerIsClose();
+        } else if (PlayerPos.x > CurrentPosition.x) {
+        	Debug.Log("1");
+        	CurrentTarget = CurrentPosition + new Vector2Int(1, 0);
+        } else if (PlayerPos.x < CurrentPosition.x) {
+        	Debug.Log("2");
+        	CurrentTarget = CurrentPosition + new Vector2Int(-1, 0);
+        } else if (PlayerPos.y > CurrentPosition.y){
+        	Debug.Log("3");
+        	CurrentTarget = CurrentPosition + new Vector2Int(0, 1);
+        } else if (PlayerPos.y < CurrentPosition.y){
+        	Debug.Log("4");
+        	CurrentTarget = CurrentPosition + new Vector2Int(0, -1);
+        }
+    }
+
+
+    private Vector2Int PlayerPos {
+    	get {
+    		return LayerManager.Instance.cachedPlayer.CurrentPosition;
+    	}
+    }
+
+    private Vector2Int CheckIfPlacerIsClose() {
+    	if (PlayerPos == CurrentPosition + new Vector2Int(1, 0)) {
+    		return CurrentPosition + new Vector2Int(1, 0);
+    	}
+    	if (PlayerPos == CurrentPosition + new Vector2Int(-1, 0)) {
+    		return CurrentPosition + new Vector2Int(1, 0);
+    	}
+    	if (PlayerPos == CurrentPosition + new Vector2Int(0, 1)) {
+    		return CurrentPosition + new Vector2Int(0, 1);
+    	}
+    	if (PlayerPos == CurrentPosition + new Vector2Int(0, -1)) {
+    		return CurrentPosition + new Vector2Int(0, -1);
+    	}
+    	return new Vector2Int(0, 0);
     }
 }
